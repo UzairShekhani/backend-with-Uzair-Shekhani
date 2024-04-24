d994beaa08f664f4f7bf5c4da6b7b1eb13340cd0
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-04-25 01:31:23 +0500

    Map Completed

--- app.js
+++ app.js
@@ -12,13 +12,19 @@
 //Array =>
 // any kind of value put in this
 // var arr = [1,2,4,5 ,"hello",true, {} ,function () {
-    
 // },[]  ]
+// var arr = [1,2,3,4,3];
+// arr.forEach(function(val) {
+//   console.log( val + 1)
+// })
 
-var arr = [1,2,3,4,3];
 
-arr.forEach(function(val) {
-  console.log( val + 1)
-})
+
+//Map
+// var arr = [1,2,3,4];
+// var newarr = arr.map(function (val) {
+//      return 13
+// })
+// console.log(newarr*arr);
 
 
