30acb6b2b9024a1b39826cfc72c1a3b70ab9e060
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-10 23:11:53 +0500

    HELLO WORLD

--- pakage.js
+++ pakage.js
@@ -7,11 +7,8 @@ const fs = require('fs');
 
 
 fs.writeFile("hey.txt", "hey hello kaisa ho",function(err) {
-    if (err) {
-        console.error(err);
-    }else{
-        console.log("done");
-    }
+    // HELLO WORLD
+    
 })
 
 //copyfile
